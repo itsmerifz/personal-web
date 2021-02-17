ef58158bbd84443140ebeb913eaff38799a8cf4d
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2021-02-17 19:25:41 +0800

    Add animation

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+new Typed('#typed',{
+    strings : ['Web Developer.','Web Designer.','C# Programmer.','Full Stack Developer.','Tech Enthusiast.'],
+    typeSpeed : 150,
+    delaySpeed : 200,
+    backSpeed : 150,
+    loop : true});
+
+var scroll = new SmoothScroll('a[href*="#"]');
+
+AOS.init();
